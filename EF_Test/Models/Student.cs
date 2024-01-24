f406f5a63ad28e9f440d17f8ef80226886a7f4d9
EbrahemElbetar1 <132098790+EbrahemElbetar1@users.noreply.github.com>
2024-01-24 23:15:16 +0200

    V_2 Create Doman Model Class

---
+++ EF_Test/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Test.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public String? Name { get; set; }
+        public int Age { get; set; }
+        public String? Email { get; set; }
+        public String? Phone { get; set; }
+        public int Grade { get; set; }
+        public DateTime Birtdate { get; set; }
+    }
+}
