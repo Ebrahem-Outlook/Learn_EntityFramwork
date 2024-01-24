8fc4caafa81f3acced2188d097816cc2a6733b87
EbrahemElbetar1 <132098790+EbrahemElbetar1@users.noreply.github.com>
2024-01-24 22:50:44 +0200

    V_1 Create SQL ConnectionString(" ")

---
+++ EF_Test/Connections.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Test
+{
+    public static class Connections
+    {
+        public const String sqlConStr = """
+            Server=DESKTOP-5PAVE27;
+            Database=DB_EF_Test;
+            User Id=sa;
+            Password=12345;
+            TrustServerCertificate=True;
+            """;
+    }
+}
